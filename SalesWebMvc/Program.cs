a079f86a84535be8dc7c412699e38ba09fdf95cc
Almir <al.mirrhrk@gmail.com>
2025-02-13 17:18:30 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddDbContext<SalesWebMvcContext>
 
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
+
 
 var app = builder.Build();
 
