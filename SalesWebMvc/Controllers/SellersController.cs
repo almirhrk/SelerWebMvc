40d40ad9ea4d1e439ae61342229a2e09efc09d33
Almir <al.mirrhrk@gmail.com>
2025-02-14 14:47:23 -0300

    Seller details and eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -74,5 +74,21 @@ namespace SalesWebMvc.Controllers
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound(nameof(Index));
+            }
+            return View(obj);
+        }
     }
 }
