b3b169ed526e7252f052e787d4e536694e3fafde
almir <al.mirrhrk@gmail.com>
2025-02-26 09:24:39 -0300

    Exception handling for delete (referential integrity)

---
+++ SalesWebMvc/Services/Exception/IntegrityException.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesWebMvc.Services.Exception
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
